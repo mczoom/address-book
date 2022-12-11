45309b6b58ed0935a38eb0d236509708f815a356
Andrew K <nscapital08@gmail.com>
2022-12-11 23:19:07 +0300

    initial commit

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Search from '../Search/Search';
+
+
+function App() {
+
+
+  return (
+    <div className='page'>
+      <div className='main'>
+        <Search />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Search/Search.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Search() {
+
+
+  return (
+    <div className='search'>
+        <input className='search__input' type='search'></input>
+        <button className='search__button'></button>
+    </div>
+  )
+}
